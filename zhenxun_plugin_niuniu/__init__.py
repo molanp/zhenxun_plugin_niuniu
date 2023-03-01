21fee57cb4681a157958f9801468ad9e20f427c4
782552619@qq.com <782552619@qq.com>
2023-03-01 22:19:15 +0800

    update version

--- zhenxun_plugin_niuniu/__init__.py
+++ zhenxun_plugin_niuniu/__init__.py
@@ -36,7 +36,7 @@ usage：
 __plugin_des__ = "牛子大作战(误"
 __plugin_type__ = ("群内小游戏",)
 __plugin_cmd__ = ["注册牛子", "jj/JJ/Jj/jJ", "我的牛子", "牛子长度排行","牛子深度排行", "打胶", "牛牛大作战"]
-__plugin_version__ = 0.2
+__plugin_version__ = 0.3
 __plugin_author__ = "molanp"
 __plugin_settings__ = {
     "level": 5,
